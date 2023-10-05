597da0c79b686385d7c2feafd19dffb80dee1af2
unknown <jknithin36@gmail.com>
2023-10-05 08:01:04 +0530

    day16

---
+++ DAY17/script.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const btn = document.querySelector('.btn-country');
+const countriesContainer = document.querySelector('.countries');
+
+///////////////////////////////////////
+
+// Notes
+// two types
+// synchronous:
+//code is executed line by line
+// Each line of code waits for previous line to finish
+// disadvantages od synchronous
+// Long-running operations block code execution
+//Asynchronous:
+// is executed after a taskw
+
+//
+//
